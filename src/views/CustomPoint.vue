845083963a738c99cb5addfefc86e05f3447f206
tangyongjie <tangyongjie@hikvision.com.cn>
2023-11-18 14:30:29 +0800

    feat: 🎸 新增鼠标跟随移动带上方向

--- src/views/CustomPoint.vue
+++ src/views/CustomPoint.vue
@@ -1,12 +1,48 @@
 <!-- 实现鼠标跟随移动 -->
 <template>
-    <div>
-        这是测试
-    </div>
+  <div ref="mouse" class="airport">
+    <img :src="airport" alt="">
+  </div>
 </template>
 
 <script setup>
+import airport from '@/assets/images/airport.svg'
+import { onBeforeMount } from 'vue';
+import { ref } from 'vue'
+import { onMounted } from 'vue';
+import { useThrottleFn } from '@vueuse/core'
+const mouse = ref(null)
+const x = ref(0)
+const y = ref(0)
+const rotate = ref(0)
+const handleMouseMove = useThrottleFn((e) => {
+  if (Math.abs(e.movementX) + Math.abs(e.movementY) > 1) {
+    rotate.value = Math.atan2(e.movementX, -e.movementY)
+  }
+  mouse.value.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`
+  mouse.value.style.transform += `rotate(${rotate.value}rad)`
+}, 10)
+onMounted(() => {
+  window.addEventListener('mousemove', handleMouseMove)
+})
+
+onBeforeMount(() => {
+  window.removeEventListener('mousemove', () => {})
+})
 
 </script>
 
-<style scoped></style>
+<style scoped>
+.airport {
+  position: fixed;
+  width: 50px;
+  height: 50px;
+  margin-left: -20px;
+  margin-top: -9px;
+}
+</style>
+<style>
+html {
+  cursor: none;
+}
+</style>
