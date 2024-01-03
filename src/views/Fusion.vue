67eac1ec9645133c28208f43419972cd81d8ac7d
tangyongjie <tangyongjie@hikvision.com.cn>
2024-01-03 15:07:18 +0800

    fix: 🐛 修改样式bug

--- src/views/Fusion.vue
+++ src/views/Fusion.vue
@@ -3,31 +3,34 @@
 </script>
 
 <template>
-  <div class="w-full h-930px flex justify-center items-center bg-black">
-    <div class="box">
-      <p class="text-#fff m-0 text-200px text">
+  <div class="w-full h-930px flex justify-center items-center bg-black box">
+    <div>
+      <span class="text-#fff m-0 text-200px text">
         Hello World
-      </p>
+      </span>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .box {
-    filter: contrast(200%);
+  filter: contrast(100);
 }
+
 .text {
-    font-weight: 700;
-    animation: fusion 2s;
+  font-weight: 700;
+  animation: fusion 2s;
 }
+
 @keyframes fusion {
-    from {
-        letter-spacing: -100px;
-        filter: blur(30px);
-    }
-    to {
-        letter-spacing: 0px;
-        filter: blur(0px);
-    }
+  from {
+    letter-spacing: -100px;
+    filter: blur(30px);
+  }
+
+  to {
+    letter-spacing: 0px;
+    filter: blur(0px);
+  }
 }
 </style>
