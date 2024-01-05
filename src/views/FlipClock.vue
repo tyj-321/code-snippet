8f5dd0c012d34cca2fc5472a757475ff4be7e684
tyj-321 <834850933@qq.com>
2024-01-05 21:07:02 +0800

    fix: 🐛 重新修改翻页时钟的效果

--- src/views/FlipClock.vue
+++ src/views/FlipClock.vue
@@ -1,101 +1,146 @@
 <script setup>
-import { computed, ref } from 'vue'
+import { ref } from 'vue'
 
-const date = ref(new Date())
-const isFlipping = ref([false, false, false, false, false, false])
+const hour = ref(0)
+const minute = ref(0)
+const second = ref(0)
+// 创建一个新的Date对象
+const currentDate = new Date()
 
-const timeArray = computed(() => {
-  const hours = date.value.getHours().toString().padStart(2, '0')
-  const minutes = date.value.getMinutes().toString().padStart(2, '0')
-  const seconds = date.value.getSeconds().toString().padStart(2, '0')
-  return [...hours, ...minutes, ...seconds]
-})
+// 获取小时、分钟、秒
+hour.value = currentDate.getHours()
+minute.value = currentDate.getMinutes()
+second.value = currentDate.getSeconds()
 
-const nextNumber = computed(() => {
-  const nextDate = new Date(date.value.getTime() + 1000)
-  const hours = nextDate.getHours().toString().padStart(2, '0')
-  const minutes = nextDate.getMinutes().toString().padStart(2, '0')
-  const seconds = nextDate.getSeconds().toString().padStart(2, '0')
-  return [...hours, ...minutes, ...seconds]
-})
+function delay(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms)
+  })
+}
 
-setInterval(() => {
-  date.value = new Date()
-  isFlipping.value = isFlipping.value.map((_, i) => timeArray.value[i] !== nextNumber.value[i])
-}, 1000)
+// 一个翻页的函数，传参是原始值，目标值
+async function flip(oldValue, newValue) {
+  // 找到原始值的卡片
+  const orignalCard = document.getElementById(`card-bottom-${oldValue}`)
+  // 设置翻转效果
+  orignalCard.style.transform = `rotateX(270deg)`
+  orignalCard.style.transformOrigin = `50% -5px`
+  orignalCard.style.transition = `transform 0.6s ease-in-out`
+  orignalCard.style.backfaceVisibility = `hidden`
+  // 找到目标值的卡片
+  const targetCard = document.getElementById(`card-top-${newValue}`)
+  await delay(300)
+  targetCard.style.zIndex = Number(newValue) + 10
+}
 </script>
 
 <template>
-  <div class="clock">
-    <div v-for="(digit, index) in timeArray" :key="index" class="flip">
-      <div class="card">
-        <div class="face top" :class="{ flip: isFlipping[index] }">
-          <div class="number">
-            {{ digit }}
+  <div class="h-700px flex items-center justify-center bg-#000 box">
+    <div class="w-800px h-600px flex justify-between items-center bg-#000 clock">
+      <div class="w-210px h-210px flex gap-10px hour">
+        <div class="w-100px h-full flex flex-col gap-10px hour-fronted">
+          <div class="relative h-100px">
+            <div v-for="i in 10" :id="`card-top-${i - 1}`" :key="i" class="absolute w-100px h-100px bg-#211f1f text-#41fe7e text-150px card-item card-item-top" :style="{ zIndex: 10 - i }">
+              <span class="absolute top">{{ i - 1 }}</span>
+            </div>
+          </div>
+          <div class="relative h-100px">
+            <div v-for="i in 10" :id="`card-bottom-${i - 1}`" :key="i" class="absolute w-100px h-100px bg-#211f1f text-#41fe7e text-150px card-item card-item-bottom" :style="{ zIndex: 10 - i }">
+              <span class="absolute bottom">{{ i - 1 }}</span>
+            </div>
           </div>
         </div>
-        <div class="face bottom" :class="{ flip: isFlipping[index] }">
-          <div class="number">
-            {{ nextNumber[index] }}
+        <div class="w-100px h-full flex flex-col gap-10px hour-end">
+          <div class="relative h-100px">
+            <div v-for="i in 10" :id="`card-top-${i - 1}`" :key="i" class="absolute w-100px h-100px bg-#211f1f text-#41fe7e text-150px card-item card-item-top" :style="{ zIndex: 10 - i }">
+              <span class="absolute top">{{ i - 1 }}</span>
+            </div>
+          </div>
+          <div class="relative h-100px">
+            <div v-for="i in 10" :id="`card-bottom-${i - 1}`" :key="i" class="absolute w-100px h-100px bg-#211f1f text-#41fe7e text-150px card-item card-item-bottom" :style="{ zIndex: 10 - i }">
+              <span class="absolute bottom">{{ i - 1 }}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="text-#fff text-30">
+        :
+      </div>
+      <div class="w-210px h-210px flex gap-10px minute">
+        <div class="w-100px h-full flex flex-col gap-10px minute-fronted">
+          <div class="relative h-100px">
+            <div v-for="i in 10" :id="`card-top-${i - 1}`" :key="i" class="absolute w-100px h-100px bg-#211f1f text-#41fe7e text-150px card-item card-item-top" :style="{ zIndex: 10 - i }">
+              <span class="absolute top">{{ i - 1 }}</span>
+            </div>
+          </div>
+          <div class="relative h-100px">
+            <div v-for="i in 10" :id="`card-bottom-${i - 1}`" :key="i" class="absolute w-100px h-100px bg-#211f1f text-#41fe7e text-150px card-item card-item-bottom" :style="{ zIndex: 10 - i }">
+              <span class="absolute bottom">{{ i - 1 }}</span>
+            </div>
+          </div>
+        </div>
+        <div class="w-100px h-full flex flex-col gap-10px minute-end">
+          <div class="relative h-100px">
+            <div v-for="i in 10" :id="`card-top-${i - 1}`" :key="i" class="absolute w-100px h-100px bg-#211f1f text-#41fe7e text-150px card-item card-item-top" :style="{ zIndex: 10 - i }">
+              <span class="absolute top">{{ i - 1 }}</span>
+            </div>
+          </div>
+          <div class="relative h-100px">
+            <div v-for="i in 10" :id="`card-bottom-${i - 1}`" :key="i" class="absolute w-100px h-100px bg-#211f1f text-#41fe7e text-150px card-item card-item-bottom" :style="{ zIndex: 10 - i }">
+              <span class="absolute bottom">{{ i - 1 }}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="text-#fff text-30">
+        <span>:</span>
+      </div>
+      <div class="w-210px h-210px flex gap-10px second">
+        <div class="w-100px h-full flex flex-col gap-10px second-fronted">
+          <div class="relative h-100px">
+            <div v-for="i in 10" :id="`card-top-${i - 1}`" :key="i" class="absolute w-100px h-100px bg-#211f1f text-#41fe7e text-150px card-item card-item-top" :style="{ zIndex: 10 - i }">
+              <span class="absolute top">{{ i - 1 }}</span>
+            </div>
+          </div>
+          <div class="relative h-100px">
+            <div v-for="i in 10" :id="`card-bottom-${i - 1}`" :key="i" class="absolute w-100px h-100px bg-#211f1f text-#41fe7e text-150px card-item card-item-bottom" :style="{ zIndex: 10 - i }">
+              <span class="absolute bottom">{{ i - 1 }}</span>
+            </div>
+          </div>
+        </div>
+        <div class="w-100px h-full flex flex-col gap-10px second-end">
+          <div class="relative h-100px">
+            <div v-for="i in 10" :id="`card-top-${i - 1}`" :key="i" class="absolute w-100px h-100px bg-#211f1f text-#41fe7e text-150px card-item card-item-top" :style="{ zIndex: 10 - i }">
+              <span class="absolute top">{{ i - 1 }}</span>
+            </div>
+          </div>
+          <div class="relative h-100px">
+            <div v-for="i in 10" :id="`card-bottom-${i - 1}`" :key="i" class="absolute w-100px h-100px bg-#211f1f text-#41fe7e text-150px card-item card-item-bottom" :style="{ zIndex: 10 - i }">
+              <span class="absolute bottom">{{ i - 1 }}</span>
+            </div>
           </div>
         </div>
       </div>
     </div>
+    <div class="w-50px h-50px bg-red" @click="flip(0, 1)">
+      翻转
+    </div>
   </div>
 </template>
 
-<style>
-.clock {
-    display: flex;
-    justify-content: center;
-}
-
-.flip {
-    perspective: 300px;
-    width: 50px;
+<style lang="scss" scoped>
+.card-item {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+  border-radius: 10px;
+  .top {
     height: 100px;
-    margin: 0 10px;
-}
-
-.card {
-    position: relative;
-    width: 100%;
-    height: 100%;
-    cursor: pointer;
-    transform-style: preserve-3d;
-    transition: transform 0.6s;
-}
-
-.face {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    backface-visibility: hidden;
-}
-
-.face.top {
-    background: #333;
-    color: white;
-    font-size: 70px;
-    line-height: 100px;
-    border-radius: 5px;
-    transform: rotateX(0deg);
-}
-
-.face.bottom {
-    background: #333;
-    color: white;
-    font-size: 70px;
-    line-height: 100px;
-    border-radius: 5px;
-    transform: rotateX(-90deg);
-}
-
-.flip .card .top.flip {
-    transform: rotateX(90deg);
-}
-
-.flip .card .bottom.flip {
-    transform: rotateX(0deg);
+  }
+  .bottom {
+    height: 100px;
+    top: -100px;
+  }
 }
 </style>
